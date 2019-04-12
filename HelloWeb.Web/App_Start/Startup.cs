f1399cafccc44cb71171221ee1f7b3eca7b3a866
bach.bx <bach.bx@gmail.com>
2019-04-13 00:21:07 +0700

    WS start

---
+++ HelloWeb.Web/App_Start/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using HelloWeb.Data;
+using HelloWeb.Data.Infrastructure;
+using HelloWeb.Data.Repositories;
+using HelloWeb.Model.Models;
+using HelloWeb.Service;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HelloWeb.Web.App_Start.Startup))]
+
+namespace HelloWeb.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Configurationfac(app);
+        }
+
+        private void Configurationfac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<HelloWebDbContext>().AsSelf().InstancePerRequest();
+
+            
+
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
+
+        }
+
+    }
+}
